f1c17e07c956c1ff1bf0c961217cade015999622
Alfonso Mateo <alfonso.mateo@tadaima.studio>
2020-05-21 17:39:24 +0200

    mech-component: basic component info

---
+++ src/components/Mech.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  #mech
+    .mech-image
+    life-display(
+      :maxLife='maxLife',
+      :currentLife='currentLife'
+    )
+</template>
+
+<script>
+// components
+import LifeDisplay from './LifeDisplay'
+export default {
+  name: 'Mech',
+  data() {
+    return {
+      currentLife: null
+    }
+  },
+
+  props: {
+    image: {
+      type: String,
+      required: true
+    },
+    maxLife: {
+      type: Number,
+      required: true
+    }
+  },
+
+  components: { LifeDisplay },
+
+  beforeMount() {
+    this.currentLife = this.maxLife
+  }
+}
+</script>
+
+<style scoped>
+#mech {
+
+}
+</style>
